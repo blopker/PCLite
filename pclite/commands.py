24dcdf3e86b0548cf90c7f8efb66788d34025297
blopker <lopkerk@gmail.com>
2013-05-21 03:06:45 -0700

    added the MIT and a pretty progress bar

--- pclite/commands.py
+++ pclite/commands.py
@@ -1,31 +1,78 @@
+'''
+The MIT License
+
+Copyright (c) Bo Lopker, http://blopker.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
 from . import logger
 log = logger.get(__name__)
+from . import settings
 from . import http
 from .lib.concurrent import futures
+import sublime
+import time
 
-executor = futures.ThreadPoolExecutor(max_workers=10)
-
-
-def command(fn):
-    def wrap(*args, **kwargs):
-        callback = args[0]
-
-        def cb(fu):
-            try:
-                callback(fu.result())
-            except Exception as e:
-                raise e
-        future = executor.submit(fn, *args, **kwargs)
-        future.add_done_callback(cb)
-    return wrap
+commandPool = futures.ThreadPoolExecutor(max_workers=10)
+progressPool = futures.ThreadPoolExecutor(max_workers=1)
 
 
-def run_command(fn, callback, *args, **kwargs):
+def command(message='Processing'):
+    def outer(fn):
+        def wrap(*args, **kwargs):
+            callback = args[0]
+            running = [True]
+
+            def show_progress(msg):
+                pos = 0
+                sym = ['-', '\\', '|', '/']
+                window = sublime.active_window()
+                view = window.active_view()
+                while running[0] is True:
+                    view = window.active_view()
+                    stat = msg + ' [' + sym[pos] + ']'
+                    view.set_status(msg, stat)
+                    pos = (pos + 1) % len(sym)
+                    time.sleep(.2)
+                view.erase_status(msg)
+
+            progressPool.submit(show_progress, message)
+
+            def cb(fu):
+                try:
+                    running[0] = False
+                    callback(fu.result())
+                except Exception as e:
+                    raise e
+            commandFuture = commandPool.submit(fn, *args, **kwargs)
+            commandFuture.add_done_callback(cb)
+        return wrap
+    return outer
+
+
+def run_command(message, fn, callback, *args, **kwargs):
     ''' Convenience function to run a command without the decorator. '''
-    command(fn)(callback, *args, **kwargs)
+    command(message)(fn)(callback, *args, **kwargs)
 
 
-@command
+@command('Getting package list')
 def get_package_list(callback):
-    j = http.getJSON('http://sublime.wbond.net/repositories.json')
+    j = http.getJSON(settings.get('repositories')[0])
     return j["repositories"]
